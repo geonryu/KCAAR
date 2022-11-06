d2483acbfb69cdc6ed634a13521fec478ddc640a
geonryu <ygun0626@gmail.com>
2022-11-07 04:05:13 +0900

    test

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ const passport = require('passport');
 const helmet = require('helmet');
 const hpp = require('hpp');
 const sanitizeHtml = require('sanitize-html');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
 dotenv.config();
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
@@ -50,8 +56,9 @@ const sessionOption = {
   cookie: {
     httpOnly: true,
     secure: false,
-  }
-}
+  },
+  store: new RedisStore({ client: redisClient }),
+};
 if(process.env.NODE_ENV === "production"){
   sessionOption.proxy = true;
   sessionOption.cookie.secure = true;
