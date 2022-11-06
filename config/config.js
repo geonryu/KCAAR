8b9b8072618253862eb99cff38acd14c701284d3
geonryu <ygun0626@gmail.com>
2022-11-07 02:43:30 +0900

    test

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "KCAAR",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: "root",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "KCAAR",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": process.env.SEQUELIZE_PASSWORD,
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: process.env.SEQUELIZE_PASSWORD,
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "KCAAR",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: "root",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "KCAAR",
+    host: "127.0.0.1",
+    dialect: "mysql"
   }
 }
