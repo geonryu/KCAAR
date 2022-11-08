87656776a084da7eba188d3f53006e76e963aba1
geonryu <ygun0626@gmail.com>
2022-11-09 02:45:35 +0900

    test

--- app.js
+++ app.js
@@ -49,6 +49,11 @@ app.use('/upload', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
 
 const sessionOption = {
   resave: false,
